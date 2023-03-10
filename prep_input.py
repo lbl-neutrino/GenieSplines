4649500859bc626e2c197e4ba2564fcfa8faf6f6
Matt Kramer <matt314159@gmail.com>
2023-03-10 15:01:38 -0800

    Make prep_input.py parse the max path file
    Thus no need to hardcode the target nuclei

--- prep_input.py
+++ prep_input.py
@@ -2,16 +2,17 @@
 
 import argparse
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 
 FLAVORS = [14, -14, 12, -12, 16, -16]
 
 # get these from the max path file
-NUCLEI = [1000050110, 1000060120, 1000070140, 1000080160, 1000110230,
-          1000120240, 1000130270, 1000140280, 1000150310, 1000160320,
-          1000170350, 1000180400, 1000190390, 1000200400, 1000220480,
-          1000240520, 1000250550, 1000260560, 1000280590, 1000300640,
-          1000822070]
+# NUCLEI = [1000050110, 1000060120, 1000070140, 1000080160, 1000110230,
+#           1000120240, 1000130270, 1000140280, 1000150310, 1000160320,
+#           1000170350, 1000180400, 1000190390, 1000200400, 1000220480,
+#           1000240520, 1000250550, 1000260560, 1000280590, 1000300640,
+#           1000822070]
 
 NUCLEONS = [1000000010, 1000010010 ]
 
@@ -20,21 +21,35 @@ def what_do_we_have(direc='splines/nuclei'):
     have = set()
 
     for xmlpath in Path(direc).glob('*.xml'):
-        parts = xmlpath.name.split('_')
+        parts = xmlpath.name.split('.')[0].split('_')
         flavor, target = int(parts[4]), int(parts[5])
-        assert flavor in FLAVORS and target in TARGETS
+        assert flavor in FLAVORS
         have.add((flavor, target))
 
     return have
+
+
+def get_nuclei(max_path):
+    root = ET.parse(max_path).getroot()
+    assert root.tag == 'path_length_list'
+    return [int(elt.get('pdgc')) for elt in root
+            if pdgc not in NUCLEONS]
 
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--mode', choices=['nucleons', 'nuclei'], required=True)
     ap.add_argument('--filter-from', help='Directory of existing splines to skip')
+    ap.add_argument('--max-path', help='Max path file (for nucleons)')
     args = ap.parse_args()
 
-    targets = NUCLEONS if args.mode == 'nucleons' else NUCLEI
+    if args.mode == 'nuclei':
+        assert args.max_path is not None, \
+            'Must specify --max-path in nuclei mode'
+        targets = get_nuclei(args.max_path)
+    else:
+        targets = NUCLEONS
+
     jobs = {(flavor, target) for flavor in FLAVORS for target in targets}
 
     if args.filter_from:
